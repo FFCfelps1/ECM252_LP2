c5de4516e8bdb16be279c0dec5f11ee2ed80eca1
Felipe Fazio <felipefazio.costa@gmail.com>
2026-03-14 10:29:31 -0300

    learn(ap004-js): Card

--- ap004-react-props/src/App.jsx
+++ ap004-react-props/src/App.jsx
@@ -1,73 +1,57 @@
+import Pedido from "./Pedido.jsx"
+import Cartao from "./Cartao.jsx"
+
 const App = () => {
   return (
     <div className="container border">
       <div className="row">
+
         <div className="col-12">
           {/* i.fa-solid.fa-hippo */}
           <i className="fa-solid fa-hippo fa-2x"></i>
         </div>
+
       </div>
       <div className="row">
-        <div className="col-sm-12 col-md-6 col col-xl-3">
-          {/* .card>(.card-header+.card-body) */}
-          <div className="card">
-            <div className="card-header text-muted">14/06/2025</div>
-            <div className="card-body d-flex">
-              <div className="d-flex align-items-center">
-                <i className="fa-solid fa-book fa-2x"></i>
-              </div>
-              {/* div>(h4.text-center+p.text-center) */}
-              <div className="border flex-grow-1 rounded p-3">
-                <h4 className="text-center">Livro</h4>
-                <p className="text-center">Um bom livro</p>
-              </div>
-            </div>
-          </div>
+        <div className="col-sm-12 col-md-6 col-xl-3">
+          <Cartao
+          cabecalho="22/03/2025">
+          <Pedido 
+            data="22/02/2026"
+            icone="camera"
+            titulo="Câmera"
+            descricao="Uma câmera 4K"/>
+            </Cartao>
         </div>
-        <div className="col-sm-12 col-md-6 col col-xl-3">
-          <div className="card">
-            <div className="card-header text-muted">11/06/2025</div>
-            <div className="card-body d-flex">
-              <div className="d-flex align-items-center">
-                <i className="fa-solid fa-pencil fa-2x"></i>
-              </div>
-              {/* div>(h4.text-center+p.text-center) */}
-              <div className="border flex-grow-1 rounded p-3">
-                <h4 className="text-center">Lápis</h4>
-                <p className="text-center">Um bom lápis</p>
-              </div>
-            </div>
-          </div>
+        <div className="col-sm-12 col-md-6 col-xl-3">
+          <Cartao
+          cabecalho="18/03/2025">
+          <Pedido 
+            data="20/02/2026"
+            icone="bicycle"
+            titulo="Bicicleta"
+            descricao="Uma bicicleta nova"/>
+          </Cartao>
         </div>
-        <div className="col-sm-12 col-md-6 col col-xl-3">
-          <div className="card">
-            <div className="card-header text-muted">10/06/2025</div>
-            <div className="card-body d-flex">
-              <div className="d-flex align-items-center">
-                <i className="fa-solid fa-bicycle fa-2x"></i>
-              </div>
-              {/* div>(h4.text-center+p.text-center) */}
-              <div className="border flex-grow-1 rounded p-3">
-                <h4 className="text-center">Bicileta</h4>
-                <p className="text-center">Uma boa bicicleta</p>
-              </div>
-            </div>
-          </div>
+        <div className="col-sm-12 col-md-6 col-xl-3">
+          <Cartao
+          cabecalho="16/02/2026">
+          <Pedido
+            data="18/02/2026"
+            icone="hippo"
+            titulo="Hipopotaamo"
+            descricao="Uma hipopotamo"/>
+          </Cartao>
         </div>
-        <div className="col-sm-12 col-md-6 col col-xl-3">
-          <div className="card">
-            <div className="card-header text-muted">14/06/2025</div>
-            <div className="card-body d-flex">
-              <div className="d-flex align-items-center">
-                <i className="fa-solid fa-hippo fa-2x"></i>
-              </div>
-              {/* div>(h4.text-center+p.text-center) */}
-              <div className="border flex-grow-1 rounded p-3">
-                <h4 className="text-center">Hipopótamo</h4>
-                <p className="text-center">Um bom Hipopótamo</p>
-              </div>
-            </div>
-          </div>
+        <div className="col-sm-12 col-md-6 col-xl-3">
+          <Cartao
+          cabecalho="16/05/2025">
+          <Pedido
+            data="16/02/2026"
+            icone="pencil"
+            titulo="Lápis"
+            descricao="Lápis novo com quatro pontas"/>
+          </Cartao>
         </div>
       </div>
     </div>
